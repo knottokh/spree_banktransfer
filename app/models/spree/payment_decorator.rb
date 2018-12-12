6dfd3da4ed2a883a47910f754ba2ebc75172a7d1
Knotto <one2treego@hotmail.com>
2018-12-12 19:34:10 +0000

    bank attachment 4

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,6 +1,9 @@
 Spree::Payment.class_eval do
   attr_accessor :validate_bank_details
   #validates :bank_attachment, presence: true, if: :validate_bank_details
+  has_attached_file :bank_attachment
+    
+  validates_attachment_content_type :bank_attachment, content_type: /\Aimage\/.*\z/ , if: :validate_bank_details
 
   scope :from_bank_transfer, -> { joins(:payment_method).where(spree_payment_methods: { type: 'Spree::PaymentMethod::BankTransfer' }) }
 
